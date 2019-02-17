1375c50c11d6969a6b952ae71b2b4aa08e5ec526
msparrow <msparrow@ucsd.edu>
2019-02-17 15:43:19 -0800

    Updated with live map

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-
 var sim = document.getElementById("sim");
 var go = document.createElement("button");
 var simarea = document.getElementById("simarea");
 var slider = document.createElement("input");
 var alert = document.createElement("img");
-//var datajson; 
-//$.getJSON("data.json", function(json) {
- // console.log("json recieved");
-//  datajson = json;
-//});
-
 
 function updateTextInput(val) {
           document.getElementById('textInput').value=val; 
@@ -26,11 +19,10 @@ var simulate = () =>{
   slider.setAttribute("min", "0");
   slider.setAttribute("max", "10");
   slider.setAttribute("step", "1");
- slider.setAttribute("min", "0");
- slider.setAttribute("min", "0");
-slider.setAttribute("value", "0")
-
-slider.setAttribute("onchange","updateTextInput(this.value);");
+  slider.setAttribute("min", "0");
+  slider.setAttribute("min", "0");
+  slider.setAttribute("value", "0")
+  slider.setAttribute("onchange","updateTextInput(this.value);");
   simarea.appendChild(slider);
   var simtext = document.createElement("input");
   simtext.setAttribute("type", "text");
@@ -50,11 +42,16 @@ slider.setAttribute("onchange","updateTextInput(this.value);");
   alert.setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/webhw2-d13bb.appspot.com/o/alert.png?alt=media&token=48030043-d029-4513-8128-d6cb583539d3");
   alert.setAttribute("alt", "Error Retrieving Alert Image");
   },5000);
+  
   setInterval(function(){
-  var audio = new Audio('./Alarm.wav');
+  var audio = new Audio('./alarm.wav');
   audio.play();
+    },5000);
+  
+  // Initialize and add the map
+
   simarea.appendChild(alert);
-  },5000);
+  
   
   
   
@@ -70,5 +67,45 @@ slider.setAttribute("onchange","updateTextInput(this.value);");
   
   
 }
+
+ // prompted by your browser. If you see the error "The Geolocation service
+      // failed.", it means you probably did not give permission for the browser to
+      // locate you.
+      var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
 
 sim.addEventListener("click", simulate)
