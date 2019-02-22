99debaf25b83039eaefee4432179182d0d35f9e0
msparrow <msparrow@ucsd.edu>
2019-02-21 18:04:28 -0800

    testing tracker name post

--- routes/trackers.js
+++ routes/trackers.js
@@ -1,3 +1,9 @@
 exports.view = function(req, res) {
     res.render('trackers');
 }
+
+exports.post= function(req, res) {
+    var tName = req.body.tName;
+    console.log(tName);   
+    res.render('trackers');
+}
