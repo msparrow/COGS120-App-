5cf66b8126782e487c2874d1e0aecb9517aa0031
msparrow <msparrow@ucsd.edu>
2019-03-14 15:09:33 -0700

    Update trackers.js

--- public/js/trackers.js
+++ public/js/trackers.js
@@ -278,13 +278,13 @@ var updateList = (data) => {
     // add Edit button for the given tracker
     var bEdit = document.createElement("button");
     bEdit.innerHTML = " Edit";
-    bEdit.color = white;
+    bEdit.color = #FFFFFF;
     bEdit.className = "editTrackerBtn";
     allTrackers.children[i].appendChild(bEdit);
 
     var bDelete = document.createElement("button");
     bDelete.innerHTML = " Delete";
-    bDelete.color = white;
+    bDelete.color = #FFFFFF;
     bDelete.className = "deleteTrackerBtn";
     allTrackers.children[i].appendChild(bDelete);
 
