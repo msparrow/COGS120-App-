ef91aefccbbbfc10a874346c8b50c70a59222acd
msparrow <msparrow@ucsd.edu>
2019-03-13 21:00:04 -0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -171,13 +171,22 @@ function initMap() {
                     map: map,
                     title: 'Hello World!'
                   });
-                  
+                  var cityCircle = new google.maps.Circle({
+                  strokeColor: '#FF0000',
+                  strokeOpacity: 0.8,
+                  strokeWeight: 2,
+                  fillColor: '#FF0000',
+                  fillOpacity: 0.35,
+                  map: map,
+                  center: campusCoords[i].center,
+                  radius: (serverData[i].range * 200)
+                    });
                    }
         
    
  
           // Add the circle for this city to the map.
-          var cityCircle = new google.maps.Circle({
+         /* var cityCircle = new google.maps.Circle({
             strokeColor: '#FF0000',
             strokeOpacity: 0.8,
             strokeWeight: 2,
@@ -186,7 +195,7 @@ function initMap() {
             map: map,
             center: myLatLng,
             radius: (serverData[0].range * 200)
-          });
+          });*/
          });
           //1500
           $.get("/trackerData",
